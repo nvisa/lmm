5eba27696047361922602f5f3accffd929b68bdf
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2012-01-20 12:39:38 +0200

    Mad: Code clean-up.

--- lmm/mad.cpp
+++ lmm/mad.cpp
@@ -14,39 +14,6 @@ Mad::Mad(QObject *parent) :
 {
 }
 
-static signed short madToShort(mad_fixed_t Fixed)
-{
-	/* A fixed point number is formed of the following bit pattern:
-	 *
-	 * SWWWFFFFFFFFFFFFFFFFFFFFFFFFFFFF
-	 * MSB                          LSB
-	 * S ==> Sign (0 is positive, 1 is negative)
-	 * W ==> Whole part bits
-	 * F ==> Fractional part bits
-	 *
-	 * This pattern contains MAD_F_FRACBITS fractional bits, one
-	 * should alway use this macro when working on the bits of a fixed
-	 * point number. It is not guaranteed to be constant over the
-	 * different platforms supported by libmad.
-	 *
-	 * The signed short value is formed, after clipping, by the least
-	 * significant whole part bit, followed by the 15 most significant
-	 * fractional part bits. Warning: this is a quick and dirty way to
-	 * compute the 16-bit number, madplay includes much better
-	 * algorithms.
-	 */
-
-	/* Clipping */
-	if(Fixed>=MAD_F_ONE)
-		return(SHRT_MAX);
-	if(Fixed<=-MAD_F_ONE)
-		return(-SHRT_MAX);
-
-	/* Conversion. */
-	Fixed=Fixed>>(MAD_F_FRACBITS-15);
-	return((signed short)Fixed);
-}
-
 static inline qint16 scale(mad_fixed_t sample)
 {
 	/* round */
@@ -66,7 +33,6 @@ int Mad::decodeAll()
 {
 	RawBuffer *buf = NULL;
 	while (inputBuffers.size()) {
-		/* TODO: free finished buffers */
 		if (buf) {
 			if (stream->next_frame) {
 				size_t left = stream->bufend - stream->next_frame;
@@ -189,51 +155,5 @@ int Mad::stopDecoding()
 int Mad::decode()
 {
 	return decodeAll();
-	if (stream->buffer == NULL || stream->error == MAD_ERROR_BUFLEN) {
-		if (inputBuffers.size() == 0)
-			return -ENOENT;
-		RawBuffer *buf = inputBuffers.takeFirst();
-		if (stream->next_frame) {
-			size_t left = stream->bufend - stream->next_frame;
-			if (buf->prepend(stream->next_frame, left))
-				mDebug("*** prepend error ***");
-		}
-		if (MAD_BUFFER_MDLEN < buf->size())
-			mDebug("chunk size is too big for mad decoder");
-		/* Pipe the new buffer content to libmad's stream decoder facility. */
-		mad_stream_buffer(stream, (const unsigned char *)buf->data(), buf->size());
-		stream->error = MAD_ERROR_NONE;
-		handleInputTimeStamps(buf);
-		delete buf;
-	}
-	if (mad_frame_decode(frame, stream)) {
-		if(MAD_RECOVERABLE(stream->error)) {
-			mDebug("recoverable frame level error (%s)", mad_stream_errorstr(stream));
-			return 0;
-		} else {
-			if(stream->error == MAD_ERROR_BUFLEN)
-				return 0;
-			mDebug("unrecoverable frame level error (%s).", mad_stream_errorstr(stream));
-			return -EIO;
-		}
-	}
-	mad_synth_frame(synth, frame);
-	if (synth->pcm.length) {
-		RawBuffer *buf = new RawBuffer;
-		buf->setSize(synth->pcm.length * 4);
-		short *data = (short *)buf->data();
-		for (int i = 0; i < synth->pcm.length; ++i) {
-			signed short lsample, rsample;
-
-			lsample = madToShort(synth->pcm.samples[0][i]);
-			rsample = madToShort(synth->pcm.samples[1][i]);
-			data[i * 2] = lsample;
-			data[i * 2 + 1] = rsample;
-		}
-		setOutputTimeStamp(buf);
-		outputBuffers << buf;
-	}
-
-	return 0;
 }
 
