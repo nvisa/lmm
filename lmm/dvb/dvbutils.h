34f3fd9dbf3e10029a066270d2005d27b0eee186
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2012-02-01 13:20:15 +0200

    Dvb: Activate DVBUtils class.

--- lmm/dvb/dvbutils.h
+++ lmm/dvb/dvbutils.h
@@ -10,49 +10,49 @@
 #include <QObject>
 
 struct dvb_ch_info {
-    QString name;
-    int freq;
-    int pol;
-    int symRate;
-    int vpid;
-    int apid;
-    int spid;
+	QString name;
+	int freq;
+	int pol;
+	int symRate;
+	int vpid;
+	int apid;
+	int spid;
 };
 
 class DVBUtils : public QObject
 {
-    Q_OBJECT
+	Q_OBJECT
 public:    
-    enum channel_pol {
-        HOR_POL = 1,
-        VER_POL = 2,
-    };
+	enum channel_pol {
+		HOR_POL = 1,
+		VER_POL = 2,
+	};
 
-    explicit DVBUtils(QObject *parent = 0);
+	explicit DVBUtils(QObject *parent = 0);
 
-    static bool tuneToChannel(QString ch);
+	static bool tuneToChannel(QString ch);
 
-    static int checkStatus();
-    static struct dvb_ch_info currentChannelInfo();
+	static int checkStatus();
+	static struct dvb_ch_info currentChannelInfo();
 
-    static QStringList channelList(QString listName = "");
-    static QStringList tvChannelList(QString listName = "");
-    static QStringList radioChannelList(QString listName = "");
+	static QStringList channelList(QString listName = "");
+	static QStringList tvChannelList(QString listName = "");
+	static QStringList radioChannelList(QString listName = "");
 
-    static QStringList fromTurksatList(QString packet = "all");
-    static void fillDbFromTurksatList();
+	static QStringList fromTurksatList(QString packet = "all");
+	static void fillDbFromTurksatList();
 
-    static QStringList satteliteLists();
-    static void selectSatteliteList(QString listName);
+	static QStringList satteliteLists();
+	static void selectSatteliteList(QString listName);
 
 signals:
 
 public slots:
 private:
-    static int zapTo();
-    static int zapTo(int freq, enum channel_pol pol, int symRate);
-    static int zapTo(QString channelName);
-    static int selectLNB(channel_pol pol, int hiband);
+	static int zapTo();
+	static int zapTo(int freq, enum channel_pol pol, int symRate);
+	static int zapTo(QString channelName);
+	static int selectLNB(channel_pol pol, int hiband);
 
 };
 
