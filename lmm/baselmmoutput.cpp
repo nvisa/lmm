ae9befb0d9ee1ee142061be2105c28b773a3ae59
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2012-02-03 19:44:45 +0200

    BaseLmmOutput: Implemenet better latency calculation.

--- lmm/baselmmoutput.cpp
+++ lmm/baselmmoutput.cpp
@@ -37,6 +37,7 @@ qint64 BaseLmmOutput::getLatency()
 
 int BaseLmmOutput::checkBufferTimeStamp(RawBuffer *buf, int jitter)
 {
+	int err = 0;
 	if (streamTime->getStartTime() == 0) {
 		streamTime->setStartTime(streamTime->getCurrentTime());
 		streamTime->setStartPts(buf->getPts());
@@ -54,23 +55,23 @@ int BaseLmmOutput::checkBufferTimeStamp(RawBuffer *buf, int jitter)
 		if ((rpts < streamDuration || streamDuration < 0) && rpts_j >= time) {
 			mInfo("it is not time to display buf %d, pts=%lld time=%lld",
 				  buf->streamBufferNo(), rpts, time);
-			last_rpts = rpts;
-			last_time = time;
-			return -1;
+			err = -1;
 		}
 	}
-	outputLatency = time - rpts_j;
-	mDebug("%s: outputLatency=%lld outputDelay=%d time=%lld rpts=%lld runningTime=%lld fps=%d", this->metaObject()->className(),
-		   outputLatency, outputDelay, time - streamTime->getStartPts(), rpts - streamTime->getStartPts(), streamTime->getFreeRunningTime(), fps);
 	last_rpts = rpts;
 	last_time = time;
-	fpsBufferCount++;
-	if (fpsTiming->elapsed() > 1000) {
-		int elapsed = fpsTiming->restart();
-		fps = fpsBufferCount * 1000 / elapsed;
-		fpsBufferCount = 0;
+	if (!err) {
+		outputLatency = time - rpts_j;
+		mDebug("%s: outputLatency=%lld outputDelay=%d time=%lld rpts=%lld runningTime=%lld fps=%d", this->metaObject()->className(),
+			   outputLatency, outputDelay, time - streamTime->getStartPts(), rpts - streamTime->getStartPts(), streamTime->getFreeRunningTime(), fps);
+		fpsBufferCount++;
+		if (fpsTiming->elapsed() > 1000) {
+			int elapsed = fpsTiming->restart();
+			fps = fpsBufferCount * 1000 / elapsed;
+			fpsBufferCount = 0;
+		}
 	}
-	return 0;
+	return err;
 }
 
 int BaseLmmOutput::outputBuffer(RawBuffer *)
@@ -86,6 +87,7 @@ int BaseLmmOutput::output()
 	if (checkBufferTimeStamp(buf)) {
 		if (doSync)
 			return -EBUSY;
+		outputLatency = 0;
 	}
 	sentBufferCount++;
 	inputBuffers.removeFirst();
