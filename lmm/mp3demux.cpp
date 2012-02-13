512687ee6ad8121c56662636d3778be9f3ebb87c
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2012-02-13 11:00:38 +0200

    Mp3Demux: Override findStreamInfo, this will be needed in future.
    This is needed for providing tag information.

--- lmm/mp3demux.cpp
+++ lmm/mp3demux.cpp
@@ -1,6 +1,15 @@
 #include "mp3demux.h"
 
+extern "C" {
+	#include "libavformat/avformat.h"
+}
+
 Mp3Demux::Mp3Demux(QObject *parent) :
 	BaseLmmDemux(parent)
 {
+}
+
+int Mp3Demux::findStreamInfo()
+{
+	return BaseLmmDemux::findStreamInfo();
 }
--- lmm/mp3demux.h
+++ lmm/mp3demux.h
@@ -8,11 +8,16 @@ class Mp3Demux : public BaseLmmDemux
 	Q_OBJECT
 public:
 	explicit Mp3Demux(QObject *parent = 0);
-	
+	QString getArtistName() { return artist; }
+	QString getAlbumName() { return album; }
 signals:
 	
 public slots:
-	
+private:
+	QString artist;
+	QString album;
+
+	int findStreamInfo();
 };
 
 #endif // MP3DEMUX_H
