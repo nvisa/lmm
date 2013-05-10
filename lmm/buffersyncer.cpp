40b80a8fe320c7d6e3c003bf74361e837aea458a
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2013-05-10 10:21:34 +0300

    BufferSyncer: Do not create thread every time, use a pool instead.
    Creating and deleting threads fails after some predefined amount of loops
    so we use a pre-allocated thread pool instead.

--- lmm/buffersyncer.cpp
+++ lmm/buffersyncer.cpp
@@ -1,9 +1,11 @@
 #include "buffersyncer.h"
 
+#include <lmm/debug.h>
 #include <lmm/streamtime.h>
 #include <lmm/baselmmoutput.h>
 
 #include <QThread>
+#include <QSemaphore>
 
 #include <errno.h>
 
@@ -15,24 +17,57 @@ public:
 		buffer = buf;
 		sleepTime = delay;
 		el = element;
+		exit = false;
+	}
+
+	BufferSyncThread()
+	{
+		exit = false;
 	}
 
 	void run()
 	{
-		usleep(sleepTime);
-		el->addBuffer(buffer);
-		/* release reference to buffer */
-		buffer = RawBuffer();
+		while (!exit) {
+			waitsem.release();
+			opsem.acquire();
+			usleep(sleepTime);
+			el->addBuffer(buffer);
+			/* release reference to buffer */
+			buffer = RawBuffer();
+		}
+	}
+
+	bool isFree()
+	{
+		return waitsem.available();
+	}
+
+	void addBuffer(BaseLmmElement *element, RawBuffer buf, qint64 delay)
+	{
+		buffer = buf;
+		sleepTime = delay;
+		el = element;
+		waitsem.acquire();
+		opsem.release();
 	}
 
 	BaseLmmElement *el;
 	RawBuffer buffer;
 	qint64 sleepTime;
+	QSemaphore opsem;
+	QSemaphore waitsem;
+	bool exit;
 };
 
-BufferSyncer::BufferSyncer(QObject *parent) :
-	BaseLmmElement(parent)
+BufferSyncer::BufferSyncer(int threadCount, QObject *parent) :
+	BaseLmmElement(parent),
+	sync(true)
 {
+	for (int i = 0; i < threadCount; i++) {
+		BufferSyncThread *th = new BufferSyncThread();
+		th->start(QThread::LowestPriority);
+		threads << th;
+	}
 }
 
 int BufferSyncer::addBuffer(BaseLmmOutput *target, RawBuffer buffer)
@@ -40,31 +75,27 @@ int BufferSyncer::addBuffer(BaseLmmOutput *target, RawBuffer buffer)
 	if (buffer.size() == 0)
 		return -EINVAL;
 
-	/* clear finished threads */
-	inputLock.lock();
-	QList<BufferSyncThread *> finished;
-	foreach(BufferSyncThread *th, threads) {
-		if (th->isFinished())
-			finished << th;
-	}
-	foreach (BufferSyncThread *th, finished) {
-		threads.removeOne(th);
-		th->deleteLater();
-	}
-	inputLock.unlock();
-
 	qint64 pts = buffer.getPts();
 	qint64 delay = streamTime->ptsToTimeDiff(pts) - target->getAvailableBufferTime();
-	if (delay > 0) {
-		BufferSyncThread *th = new BufferSyncThread(target, buffer, delay);
-		th->start(QThread::LowestPriority);
-		inputLock.lock();
-		threads << th;
-		inputLock.unlock();
+	if (sync && delay > 0) {
+		BufferSyncThread *th = findEmptyThread();
+		if (th)
+			th->addBuffer(target, buffer, delay);
+		else
+			mDebug("cannot find an empty thread, skipping");
 	} else {
 		target->addBuffer(buffer);
 		receivedBufferCount++;
 	}
 
 	return 0;
+}
+
+BufferSyncThread * BufferSyncer::findEmptyThread()
+{
+	foreach(BufferSyncThread *th, threads) {
+		if (th->isFree())
+			return th;
+	}
+	return NULL;
 }
--- lmm/buffersyncer.h
+++ lmm/buffersyncer.h
@@ -10,13 +10,17 @@ class BufferSyncer : public BaseLmmElement
 {
 	Q_OBJECT
 public:
-	explicit BufferSyncer(QObject *parent = 0);
+	explicit BufferSyncer(int threadCount = 20, QObject *parent = 0);
 	int addBuffer(BaseLmmOutput *target, RawBuffer buffer);
+	void setSyncEnabled(bool en) { sync = en; }
+	bool syncEnabled() { return sync; }
 signals:
 	
 public slots:
 protected:
+	bool sync;
 	QList<BufferSyncThread *> threads;
+	BufferSyncThread * findEmptyThread();
 };
 
 #endif // BUFFERSYNCER_H
