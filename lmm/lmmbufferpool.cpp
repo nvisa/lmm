0ad66e7a77bdc2ae07d8c2887b05c9ba8c7e515a
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2013-07-30 11:18:36 +0300

    lmm: LmmBufferPool: This class is a work in progress.
    Add some documentation to clarify working mechanism.

--- lmm/lmmbufferpool.cpp
+++ lmm/lmmbufferpool.cpp
@@ -1,5 +1,26 @@
 #include "lmmbufferpool.h"
 
+/**
+	\class LmmBufferPool
+
+	\brief Bu sinif bir tampon havuzu sunar. Bu sinif ile belirli bir sayida
+	tamponu kutuphane icinde farkli noktalarda kullanmak icin kullanabilirsiniz.
+
+	LmmBufferPool sinifinin 2 kullanim sekli mevcuttur, tampon-yonetimli ya da
+	kaynak-yonetimli. Tampon-yonetimli kullanim seklinde, LmmBufferPool nesnesini
+	yaratirken yapilandirici fonksiyona bir sayi gecebilirsiniz. Bu durumda
+	LmmBufferPool o sayida RawBuffer olusturacak ve tamponlarin sahibini kendisi
+	olarak isaretleyecektir. Bu sayede tampon yonetimini kendisi saglayacaktir.
+	Bu noktadan sonra tek yapmaniz gereken take() fonksiyonunu kullanarak
+	yeni bir tampon almanizdir. Eger havuz icinde tampon yoksa take() fonksiyonu
+	yeni bir tampon gelene kadar block olacaktir. Bu sekilde aldiginiz tamponlari
+	geri vermenize gerek yoktur, sistemde tamponu kullanan kimse kalmadigi zaman
+	tampon otomatik olarak havuza dusecektir.
+
+	Kaynak-yonetimli bicimde ise
+
+*/
+
 LmmBufferPool::LmmBufferPool(QObject *parent) :
 	QObject(parent)
 {
