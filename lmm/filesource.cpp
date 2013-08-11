f349cb4fa1c460e71389d6fd2f998f4c85b8b343
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2013-08-11 17:04:41 +0300

    FileSource: Implement basic functionality.

--- lmm/filesource.cpp
+++ lmm/filesource.cpp
@@ -1,6 +1,53 @@
 #include "filesource.h"
 
+#include <lmm/debug.h>
+#include <lmm/circularbuffer.h>
+
+#include <QFile>
+
+#include <errno.h>
+
 FileSource::FileSource(QObject *parent) :
-	QObject(parent)
+	BaseLmmElement(parent)
 {
+	packState = 0;
+}
+
+int FileSource::read(int size)
+{
+	RawBuffer buf("unknown", size);
+	fileLock.lock();
+	int len = -1;
+	if (file->isOpen())
+		len = file->read((char *)buf.data(), size);
+	fileLock.unlock();
+	if (len <= 0)
+		return len;
+	else if (len < size) {
+		RawBuffer buf2("unknown", len);
+		memcpy(buf2.data(), buf.data(), len);
+		buf = buf2;
+	}
+	outputLock.lock();
+	outputBuffers << buf;
+	releaseOutputSem(0);
+	outputLock.unlock();
+	return len;
+}
+
+int FileSource::start()
+{
+	file = new QFile(filename);
+	if (!file->open(QIODevice::ReadOnly))
+		return -ENOENT;
+	return BaseLmmElement::start();
+}
+
+int FileSource::stop()
+{
+	fileLock.lock();
+	if (file->isOpen())
+		file->close();
+	fileLock.unlock();
+	return BaseLmmElement::stop();
 }
--- lmm/filesource.h
+++ lmm/filesource.h
@@ -1,18 +1,30 @@
 #ifndef FILESOURCE_H
 #define FILESOURCE_H
 
+#include <QMap>
 #include <QObject>
 
-class FileSource : public QObject
+#include <lmm/baselmmelement.h>
+
+class QFile;
+
+class FileSource : public BaseLmmElement
 {
 	Q_OBJECT
 public:
 	explicit FileSource(QObject *parent = 0);
-	
+	void setFilename(QString fn) { filename = fn; }
+	int read(int size = 4096);
+	virtual int start();
+	virtual int stop();
+
 signals:
 	
 public slots:
-	
+protected:
+	QString filename;
+	QFile *file;
+	QMutex fileLock;
 };
 
 #endif // FILESOURCE_H
