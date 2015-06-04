82c43cc800cfe579eba261d8c40a8c4c28958b77
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2015-06-04 11:47:34 +0300

    tools: video: Add support for 2 pixel formats.

--- lmm/tools/videoutils.cpp
+++ lmm/tools/videoutils.cpp
@@ -8,6 +8,8 @@ VideoUtils::VideoUtils()
 
 int VideoUtils::getLineLength(int pixFmt, int width)
 {
+	if (pixFmt == V4L2_PIX_FMT_YUYV)
+		return width * 2;
 	if (pixFmt == V4L2_PIX_FMT_UYVY)
 		return width * 2;
 	if (pixFmt == V4L2_PIX_FMT_NV12)
@@ -18,12 +20,14 @@ int VideoUtils::getLineLength(int pixFmt, int width)
 		return width;
 	if (pixFmt == V4L2_PIX_FMT_SGRBG10)
 		return width * 2;
+	if (pixFmt == V4L2_PIX_FMT_YUV420)
+		return width;
 	return 0;
 }
 
 int VideoUtils::getFrameSize(int pixFmt, int width, int height)
 {
-	if (pixFmt == V4L2_PIX_FMT_NV12)
+	if (pixFmt == V4L2_PIX_FMT_NV12 || pixFmt == V4L2_PIX_FMT_YUV420)
 		return width * height * 3 / 2;
 	return getLineLength(pixFmt, width) * height;
 }
