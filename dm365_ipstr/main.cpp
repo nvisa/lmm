1cbb279352d92a1df279724b13a53755df9a1fc3
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2016-04-08 11:42:39 +0300

    dm365_ipstr: Activate analog encoder streaming functionality.

--- dm365_ipstr/main.cpp
+++ dm365_ipstr/main.cpp
@@ -1,6 +1,7 @@
 #include <QStringList>
 #include <QCoreApplication>
 
+#include <lmm/dm365/cvbsstreamer.h>
 #include <lmm/dm365/ipcamerastreamer.h>
 #include <lmm/dm365/simplertpstreamer.h>
 #include <lmm/dm365/simple1080pstreamer.h>
@@ -14,7 +15,9 @@ int main(int argc, char *argv[])
 	BaseStreamer *s = NULL;
 	if (a.arguments().contains("--1080p"))
 		s = new Simple1080pStreamer;
-	 else
+	else if (a.arguments().contains("--cvbs"))
+		s = new CVBSStreamer;
+	else
 		s = new IPCameraStreamer;
 	s->start();
 
