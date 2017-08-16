e12ff69f94b4aa832ac96428a2472610de460270
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2017-08-16 22:15:20 +0300

    plaform_info: Improve compile-time arch detection.

--- lmm/platform_info.h
+++ lmm/platform_info.h
@@ -1,12 +1,30 @@
 #ifndef PLATFORM_INFO_H
 #define PLATFORM_INFO_H
 
-#if defined(__arm__)
+#if defined(__ARM_ARCH_5__) || defined(__ARM_ARCH_5E__) || defined(__ARM_ARCH_5T__) || defined(__ARM_ARCH_5TE__) || defined(__ARM_ARCH_5TEJ__)
 	#define cpu_is_x86() 0
 	#define cpu_is_arm() 1
+	#define cpu_is_armv5() 1
+	#define cpu_is_armv7() 1
+	#define cpu_is_armv64() 0
+#elif defined(__ARM_ARCH_7__) || defined(__ARM_ARCH_7A__) || defined(__ARM_ARCH_7R__) || defined(__ARM_ARCH_7M__) || defined(__ARM_ARCH_7S__)
+	#define cpu_is_x86() 0
+	#define cpu_is_arm() 1
+	#define cpu_is_armv5() 0
+	#define cpu_is_armv7() 1
+	#define cpu_is_armv64() 0
+#elif defined(__aarch64__)
+	#define cpu_is_x86() 0
+	#define cpu_is_arm() 1
+	#define cpu_is_armv5() 0
+	#define cpu_is_armv7() 0
+	#define cpu_is_armv64() 1
 #else
 	#define cpu_is_x86() 1
 	#define cpu_is_arm() 0
+	#define cpu_is_armv5() 0
+	#define cpu_is_armv7() 0
+	#define cpu_is_armv64() 0
 #endif
 
 #endif // PLATFORM_INFO_H
