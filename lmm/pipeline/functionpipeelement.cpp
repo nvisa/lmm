bd973357f554a85e56c2f06a7cddd57039b8fd56
Yusuf Caglar Akyuz <caglar@bilkon-kontrol.com.tr>
2017-09-17 08:59:12 +0300

    pipeline: Implement long waited function pipe element.
    This element helps you to use ordinary functions or class
    members as a pipeline element.

--- lmm/pipeline/functionpipeelement.cpp
+++ lmm/pipeline/functionpipeelement.cpp
@@ -1,2 +1,27 @@
 #include "functionpipeelement.h"
 
+FunctionPipeElement::FunctionPipeElement(FunctionPipeElement::processFunc func, QObject *parent)
+	: BaseLmmElement(parent)
+{
+	ftarget = func;
+	otarget = NULL;
+}
+
+FunctionPipeElement::FunctionPipeElement(PipeElementoid *eoid, QObject *parent)
+	: BaseLmmElement(parent)
+{
+	ftarget = NULL;
+	otarget = eoid;
+}
+
+int FunctionPipeElement::processBuffer(const RawBuffer &buf)
+{
+	if (ftarget) {
+		int err = (*ftarget)(buf);
+		if (err)
+			return err;
+		return newOutputBuffer(buf);
+	}
+
+	return otarget->processBuffer(buf);
+}
--- lmm/pipeline/functionpipeelement.h
+++ lmm/pipeline/functionpipeelement.h
@@ -1,8 +1,53 @@
 #ifndef FUNCTIONPIPEELEMENT_H
 #define FUNCTIONPIPEELEMENT_H
 
-#include <lmm/pipeline/basepipeelement.h>
+#include <lmm/baselmmelement.h>
 
+class PipeElementoid
+{
+public:
+	virtual int processBuffer(const RawBuffer &buf) = 0;
+	virtual ~PipeElementoid() = 0;
+};
 
+template<class T>
+class GenericPipeElementoid : public PipeElementoid
+{
+public:
+	typedef int (T::*pipeOp)(const RawBuffer &buf);
+
+	GenericPipeElementoid(T *c, pipeOp member)
+	{
+		this->c = c;
+		func = member;
+	}
+
+	int processBuffer(const RawBuffer &buf)
+	{
+		return (c->*func)(buf);
+	}
+
+protected:
+	T *c;
+	pipeOp func;
+};
+
+inline PipeElementoid::~PipeElementoid() {}
+
+class FunctionPipeElement : public BaseLmmElement
+{
+	Q_OBJECT
+public:
+	typedef int (*processFunc)(const RawBuffer &buf);
+
+	FunctionPipeElement(processFunc func, QObject *parent = NULL);
+	FunctionPipeElement(PipeElementoid *eoid, QObject *parent = NULL);
+
+protected:
+	virtual int processBuffer(const RawBuffer &buf);
+
+	processFunc ftarget;
+	PipeElementoid *otarget;
+};
 
 #endif // FUNCTIONPIPEELEMENT_H
